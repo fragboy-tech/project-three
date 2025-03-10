387676943a358e6255499ef2a993469100f2041a
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2025-03-10 17:51:52 +0800

    up

--- db/models/Com.ts
+++ db/models/Com.ts
@@ -4,26 +4,16 @@ import { comSchema } from "../schema/comSchema";
 interface ICom extends Document {
   content: string;
 }
-interface ComModel extends Model<ICom> {}
-getComment({
-
-    content,
-}{
-    content:string;
-}):Promise<ICom>
+interface ComModel extends Model<ICom> {
+  getComment({ content }: { content: string }): Promise<ICom>;
+}
 
 class Com {
-    static async getComment(
-        this:ComModel,
-            {
-            content,
-        }:{content:string}
-    ):Promise<ICom>
-
+  static async getComment(
+    this: ComModel,
+    { content }: { content: string }
+  ): Promise<ICom>;
 }
 comSchema.loadClass(Com);
 
-export const Coms:ComModel=mongoose.model<ICom,ComModel>(
-    "Com",
-    comSchema
-);
+export const Coms: ComModel = mongoose.model<ICom, ComModel>("Com", comSchema);
